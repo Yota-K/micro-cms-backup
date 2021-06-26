4a3ef2186387668768e6bd93533806eaa95d2919
Y.K <powdersugar828828@gmail.com>
2021-06-27 03:08:31 +0900

    feat: 取得したJSONをループして表示できるようにして。

--- app/main.go
+++ app/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -9,20 +10,37 @@ import (
 	"github.com/micro-cms-backup/config"
 )
 
+// MEMO: サムネイル・カテゴリー・タグは一旦後回し
+type Content struct {
+	Id          string `json:"id"`
+	Title       string `json:"title"`
+	Body        string `json:"body"`
+	Description string `json:"description"`
+}
+
+type MicroCmsBlogs struct {
+	Contents []Content `json:"contents"`
+}
+
 func main() {
-	result, err := reqApi()
+	API_KEY, ENDPOINT := config.Init()
+	if API_KEY == "" || ENDPOINT == "" {
+		log.Fatal("API_KEYかエンドポイントが設定されていません")
+	}
+
+	result, err := reqApi(API_KEY, ENDPOINT)
 	if err != nil {
 		log.Fatal(err)
 		panic(err.Error())
 	}
-	fmt.Println(string(*result))
+
+	createCsv(result)
 }
 
-func reqApi() (*[]byte, error) {
-	API_KEY, ENDPOINT := config.Init()
-
+func reqApi(API_KEY, ENDPOINT string) (*[]byte, error) {
 	// リクエストを作成
-	req, err := http.NewRequest("GET", fmt.Sprintf("%s/blogs", ENDPOINT), nil)
+	params := "blogs?fields=id,title,body,description&limit=9999"
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s/%s", ENDPOINT, params), nil)
 	if err != nil {
 		log.Fatal(err)
 		return nil, err
@@ -42,4 +60,21 @@ func reqApi() (*[]byte, error) {
 	byteAry, err := ioutil.ReadAll(res.Body)
 
 	return &byteAry, err
+}
+
+func createCsv(jsonBytes *[]byte) {
+	blogs := new(MicroCmsBlogs)
+
+	// 構造体blogsにJSONのバイト列を格納する
+	if err := json.Unmarshal(*jsonBytes, blogs); err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	// MEMO: ループ内でCSVの書き込みを行うイメージ
+	for _, blog := range blogs.Contents {
+		fmt.Printf("id: %s\n", blog.Id)
+		fmt.Printf("title: %s\n", blog.Title)
+		fmt.Print("\n")
+	}
 }
